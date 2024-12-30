504ce157cc09e5271cb5602a8c6ad5a9a72024aa
emonte9 <montero.edgar1997@gmail.com>
2024-12-29 21:37:18 -0600

    '"pass"

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            ProgressView(value: 5, total:15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.filled")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.filled")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }.accessibilityLabel("Next Speaker")
+            }
+            
+            
+        }.padding()
+       
     }
 }
 
